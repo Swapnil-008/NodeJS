fd29d4ea3d8c86b78be342e07a9aeacd037b6b31
Swapnil <swapnilshingne155@gmail.com>
2025-10-03 14:22:00 +0530

    Add initial HTTP methods documentation

---
+++ 08_HTTP_Methods/index.js
@@ -0,0 +1,9 @@
+// HTTP Requests
+
+//GET - When you want to get some data from the server.
+//POST - When you want to send some data in server.
+//PUT - When you want to upload something.
+//PATCH - When you want to update the existing data.
+//DELETE - When you want to delete someting from the server.
+
+//req.method -> returns the type of method
