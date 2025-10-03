2d35bbf863a82418f996e7ea14f7f806c0dcc309
Swapnil <swapnilshingne155@gmail.com>
2025-10-03 14:04:10 +0530

    Adding code of 08 lecture

--- 07_Building_HTTP_Server/index.js
+++ 07_Building_HTTP_Server/index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const { listenerCount } = require('process')
 const fs = require('fs')
+const url = require('url')
 
 fs.writeFileSync('./text.txt', "New File Created\n")
 
@@ -12,15 +13,23 @@ const myServer = http.createServer((req, res) => {
     //     res.end("Hello from server!")
     // })
     // }
-    const content = `New Request Added!, Time: ${new Date().toLocaleString()}, ${req.url} \n`
+    const content = `New Request Added!, Time: ${new Date().toLocaleString()}, ${req.url} \n`;
+    const myUrl = url.parse(req.url, true) // This true allows to create a object and seprate all the query parameters to that query
+    // Ex path = path: '/about?myname=swapnil&id=43178&year=4',
+    // In myUrl there is property query = query: [Object: null prototype] {
+    //     myname: 'swapnil',
+    //     id: '43178',
+    //     year: '4'
+    //   },
+    console.log(myUrl)
     fs.appendFile('./text.txt', content, (err, data) => {
-        if(req.url === '/')
+        if(myUrl.pathname === '/')
         {
             res.end("Welcome to Home!")
         }
-        else if(req.url === '/about')
+        else if(myUrl.pathname === '/about')
         {
-            res.end("Welcome to About!")
+            res.end(`Hey, ${myUrl.query.myname}`)
         }
         else{
             res.end("Welcome to Favicon!")
