668d054e25b8825eab5883ebf3cf6651a8e8bbb8
Swapnil <swapnilshingne155@gmail.com>
2025-11-19 23:05:41 +0530

    Add comments to clarify the purpose of require() and module imports

--- 04_Modules/index.js
+++ 04_Modules/index.js
@@ -1,3 +1,8 @@
+// require() is a function in Node.js that is used to import things.
+// Inbuilt Node packages
+// Packages installed via npm
+// Your own local files
+
 const {add, sub} = require('./math')
 
 console.log(`Add: ${add(10, 15)}, Sub: ${sub(25, 10)}`)
